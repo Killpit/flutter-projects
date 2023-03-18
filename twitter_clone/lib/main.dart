516b0a86b34b3558ae87d55d1584e8cfa8e06e0b
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-18 21:46:46 +0000

    19 Mar 2023 00:46

--- twitter_clone/lib/main.dart
+++ twitter_clone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Twitter Clone',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: AppTheme.theme,
       home: const MyHomePage(title: 'Twitter Clone Home Page'),
     );
   }

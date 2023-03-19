97836396e26aefecab703a21980fa4dea366d28c
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-19 18:29:17 +0000

    19 Mar 2023 21:29

--- twitter_clone/lib/constants/ui_constants.dart
+++ twitter_clone/lib/constants/ui_constants.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/constants/assets_constants.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:twitter_clone/theme/pallete.dart';
 
 class UIConstants {
   static AppBar appBar() {
     return AppBar(
       title: SvgPicture.asset(AssetsConstants.twitterLogo,
+      color: Pallete.blueColor,
       ),
     );
   }

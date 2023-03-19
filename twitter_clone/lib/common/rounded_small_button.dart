950bf814ce47973c0ceb12ce834edee4f595d38d
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-19 19:21:12 +0000

    19 Mar 2023 22:21

---
+++ twitter_clone/lib/common/rounded_small_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoundedSmallButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const RoundedSmallButton({
+    super.key,
+    required this.onTap,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

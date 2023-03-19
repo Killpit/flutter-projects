132aa5462080431e85f47abad734105b89ae7573
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-19 18:46:24 +0000

    19 Mar 2023 21:46

---
+++ twitter_clone/lib/features/auth/widgets/auth_field.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthField extends StatelessWidget {
+  final TextEditingController controller;
+  const AuthField({super.key, required this.controller, });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      
+    );
+  }
+}

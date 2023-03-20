d47228f13d40da4abce02e312853a492ef9e9d39
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-20 15:46:33 +0000

    20 Mar 2023 18:46

--- twitter_clone/lib/features/auth/view/signup_view.dart
+++ twitter_clone/lib/features/auth/view/signup_view.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/common/rounded_small_button.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
+
+import '../../../theme/pallete.dart';
 
 class SignUpView extends StatefulWidget {
   const SignUpView({super.key});
@@ -22,6 +27,56 @@ class _SignUpViewState extends State<SignUpView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appbar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              //textfield1
+              AuthField(
+                controller: emailController,
+                hintText: 'Email',
+              ),
+              const SizedBox(height: 25),
+
+              AuthField(
+                controller: passwordController,
+                hintText: 'Password',
+              ),
+              const SizedBox(height: 40),
+              //button
+              Align(
+                alignment: Alignment.topRight,
+                child: RoundedSmallButton(onTap: () {}, label: 'Done', 
+                ),
+              ),
+              const SizedBox(height: 40),
+              RichText(text: TextSpan(
+                text: "Don't have an account?",
+                style: const TextStyle(
+                  fontSize: 16,
+                    ),
+                children: [
+                  TextSpan(
+                    text: ' Sign up',
+                    style: const TextStyle(
+                      color: Pallete.blueColor,
+                      fontSize: 16,
+                    ),
+                    recognizer: TapGestureRecognizer()..onTap = () {
+
+                    },
+                  ),
+                ],
+              ),
+              ),
+            ],
+          )
+          ),
+        ),
+      ),
+    );
   }
 }

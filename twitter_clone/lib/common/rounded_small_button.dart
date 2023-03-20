0f5b6979f475abfc282ce10e82d7dab4d6af04bc
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-20 15:21:23 +0000

    20 Mar 2023 18:21

--- twitter_clone/lib/common/rounded_small_button.dart
+++ twitter_clone/lib/common/rounded_small_button.dart
@@ -11,17 +11,22 @@ class RoundedSmallButton extends StatelessWidget {
     super.key,
     required this.onTap,
     required this.label,
-    this.backgroundColor = Pallete.backgroundColor,
-    this.textColor = Pallete.whiteColor,
+    this.backgroundColor = Pallete.whiteColor,
+    this.textColor = Pallete.backgroundColor,
     });
 
   @override
   Widget build(BuildContext context) {
     return Chip(
       label: Text(label,
-      style: TextStyle(color: textColor),
+      style: TextStyle(color: textColor,
+      fontSize: 16,
+      ),
       ),
       backgroundColor: backgroundColor,
+      labelPadding: const EdgeInsets.symmetric(horizontal: 20,
+      vertical: 5,
+      ),
     );
   }
 }

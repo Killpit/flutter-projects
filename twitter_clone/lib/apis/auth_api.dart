fad10fa8de64ed23feadcae25d0a9a00a47021b4
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-22 16:30:15 +0000

    22 Mar 2023 19:30

--- twitter_clone/lib/apis/auth_api.dart
+++ twitter_clone/lib/apis/auth_api.dart
@@ -1,9 +1,12 @@
 import 'package:twitter_clone/core/core.dart';
-import 'package:appwrite/models.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:appwrite/appwrite.dart';
 
+//want to signup, want to get user account -> Account
+//want to access user related data ->model.Account
 
 abstract class IAuthAPI {
-  FutureEither<Account> signUp({
+  FutureEither<model.Account> signUp({
     required String email,
     required String password,
   });

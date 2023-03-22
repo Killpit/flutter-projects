2a9f83554428782e8476c80d0a3dcf223e017eb6
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-22 16:06:54 +0000

    22 Mar 2023 19:06

--- twitter_clone/lib/core/type_defs.dart
+++ twitter_clone/lib/core/type_defs.dart
@@ -2,3 +2,4 @@ import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/core/failure.dart';
 
 typedef FutureEither<T> = Future<Either<Failure, T>>;
+typedef FutureEitherVoid = FutureEither<void>;

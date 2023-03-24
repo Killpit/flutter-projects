206dc951fdc0dce34711c7ff973251fc4b441b1b
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-24 19:09:00 +0000

    24 Mar 2023 22:09

--- twitter_clone/lib/main.dart
+++ twitter_clone/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_clone/features/auth/view/login_view.dart';
 import 'package:twitter_clone/features/auth/view/signup_view.dart';
 import 'package:twitter_clone/theme/theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(ProviderScope(child: const MyApp()));
+  runApp(const ProviderScope(child: MyApp(),
+  ),
+  );
 }
 
 class MyApp extends StatelessWidget {

d41dbd7a09713ac86b4b29e5ca77df2978ba9240
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-24 18:19:58 +0000

    24 Mar 2023 21:19

--- twitter_clone/lib/features/auth/controller/auth_controller.dart
+++ twitter_clone/lib/features/auth/controller/auth_controller.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/apis/auth_api.dart';
 
 class AuthController extends StateNotifier<bool> {
-  AuthController(): super(false);
-  
-  //isLoading
-  void signUp({
+  final AuthAPI _authAPI;
+  AuthController({required AuthAPI authAPI}): _authAPI = authAPI, super(false);
+  //state = isLoading
+
+  Future<void> signUp({
     required String email,
     required String password,
     required BuildContext context,
-  }) {}
+  }) async {
+    state = true;
+    final res = await _authAPI.signUp(
+      email: email, 
+      password: password,
+      );
+  }
 }

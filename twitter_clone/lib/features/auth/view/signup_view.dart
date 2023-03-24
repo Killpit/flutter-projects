6aa97175cd75f95ec597d4bad63193f959a92d32
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-24 18:38:59 +0000

    24 Mar 2023 21:38

--- twitter_clone/lib/features/auth/view/signup_view.dart
+++ twitter_clone/lib/features/auth/view/signup_view.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/features/auth/view/login_view.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
-class SignUpView extends StatefulWidget {
+class SignUpView extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
     builder: (contest) => const SignUpView(),
   );
   const SignUpView({super.key});
 
   @override
-  State<SignUpView> createState() => _SignUpViewState();
+  ConsumerState<SignUpView> createState() => _SignUpViewState();
 }
 
-class _SignUpViewState extends State<SignUpView> {
+class _SignUpViewState extends ConsumerState<SignUpView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -29,7 +30,7 @@ class _SignUpViewState extends State<SignUpView> {
   }
 
   void onSignUp() {
-    
+
   }
 
   @override

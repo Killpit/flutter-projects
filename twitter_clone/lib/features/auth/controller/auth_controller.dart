9741804a09af6127a38f7d8c12290a6677217708
Ata Tekeli <tekeliata2000@gmail.com>
2023-03-24 18:15:34 +0000

    24 Mar 2023 21:15

--- twitter_clone/lib/features/auth/controller/auth_controller.dart
+++ twitter_clone/lib/features/auth/controller/auth_controller.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AuthController extends StateNotifier {
-  AuthController(super.state);
+class AuthController extends StateNotifier<bool> {
+  AuthController(): super(false);
   
+  //isLoading
+  void signUp({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) {}
 }

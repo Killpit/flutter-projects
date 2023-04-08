b067cf331ab926b97f6ecf04213a9a771117f3d2
Ata Tekeli <tekeliata2000@gmail.com>
2023-04-08 21:04:56 +0000

    9 Nis 2023 00:04

--- twitter_clone/lib/apis/auth_api.dart
+++ twitter_clone/lib/apis/auth_api.dart
@@ -59,10 +59,10 @@ class AuthAPI implements IAuthAPI {
     required String password,
     }) async {
     try {
-      final account = await _account.createEmailSession(
+      final session = await _account.createEmailSession(
       email: email, 
       password: password);
-      return right(account);
+      return right(session);
     } on AppwriteException catch (e, stackTrace) {
       return left(
         Failure(e.message ?? 'Some unexpected error occurred', 

41e36d526364a141a360be1d33490858bc841a4c
Ata Tekeli <tekeliata2000@gmail.com>
2023-04-08 21:13:00 +0000

    9 Nis 2023 00:13

--- twitter_clone/lib/features/auth/view/login_view.dart
+++ twitter_clone/lib/features/auth/view/login_view.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/rounded_small_button.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/features/auth/view/signup_view.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
-class LoginView extends StatefulWidget {
+class LoginView extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
                         builder: (context) => const LoginView(),
                       );
   const LoginView({super.key});
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  ConsumerState<LoginView> createState() => _LoginViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _LoginViewState extends ConsumerState<LoginView> {
   final appbar = UIConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -26,6 +28,15 @@ class _LoginViewState extends State<LoginView> {
     super.dispose();
     emailController.dispose();
     passwordController.dispose();
+  }
+
+  void onLogin() {
+    final res = ref.read(authControllerProvider.notifier)
+    .signUp(
+      email: emailController.text, 
+      password: passwordController.text, 
+      context: context,
+      );
   }
 
   @override

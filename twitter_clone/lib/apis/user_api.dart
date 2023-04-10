e47a16fed6b2a8eff7664da3399717cca08e25fb
Ata Tekeli <tekeliata2000@gmail.com>
2023-04-10 16:01:04 +0000

    10 Nis 2023 19:01

--- twitter_clone/lib/apis/user_api.dart
+++ twitter_clone/lib/apis/user_api.dart
@@ -3,3 +3,11 @@ import 'package:twitter_clone/core/core.dart';
 abstract class IUserAPI {
   FutureEitherVoid saveUserData();
 }
+
+class UserAPI implements IUserAPI {
+  @override
+  FutureEitherVoid saveUserData() {
+    // TODO: implement saveUserData
+    throw UnimplementedError();
+  }
+}

273c532b9577a148c0c9f86e4f1cb648ad08a7bb
Ata Tekeli <tekeliata2000@gmail.com>
2023-04-12 11:30:05 +0000

    12 Nis 2023 14:30

--- twitter_clone/lib/constants/appwrite_constants.dart
+++ twitter_clone/lib/constants/appwrite_constants.dart
@@ -2,4 +2,5 @@ class AppwriteConstants {
   static const String databaseId = '641628e27771bf6c4fbc';
   static const String projectId = '641486a42bd0ee4249b4';
   static const String endPoint = 'http://localhost:80';
+  static const String usersCollection = '64369282c8f4135d170c';
 }

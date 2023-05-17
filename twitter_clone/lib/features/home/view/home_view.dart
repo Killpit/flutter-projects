fd1a79eb83bee7c72b53f0e2d37390cc6809e121
Ata Tekeli <tekeliata2000@gmail.com>
2023-05-17 18:35:05 +0000

    17 May 2023 21:35

--- twitter_clone/lib/features/home/view/home_view.dart
+++ twitter_clone/lib/features/home/view/home_view.dart
@@ -1,14 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
+import 'package:twitter_clone/constants/ui_constants.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends StatefulWidget {
   static route() => MaterialPageRoute(
     builder: (context) => const HomeView(),
     );
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final appBar = UIConstants.appBar();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      bottomNavigationBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(AssetsConstants.homeFilledIcon,),
+          ),
+        ],
+      ),
+    );
   }
 }

56d387c52bfd8c23bde7823557f4d85a6ddf7489
Ata Tekeli <tekeliata2000@gmail.com>
2023-05-19 18:52:41 +0000

    19 May 2023 21:52

--- twitter_clone/lib/features/tweet/controller/tweet_controller.dart
+++ twitter_clone/lib/features/tweet/controller/tweet_controller.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/core/utils.dart';
 
 class TweetController extends StateNotifier<bool> {
   TweetController(): super(false);
@@ -10,6 +10,20 @@ class TweetController extends StateNotifier<bool> {
     required List<File> images,
     required String text,
     required BuildContext context,
+  }) {
+    if(text.isEmpty) {
+      showSnackBar(context, 'Please enter text');
+      return;
+    }
+
+    if(images.isNotEmpty) {
+
+    }
+  }
+
+  void _shareImageTweet({
+    required List<File> images,
+    required String text,
   }) {
     
   }

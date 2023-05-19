3b09c6f84ce8d84683608dc57bb2d0fedffaf924
Ata Tekeli <tekeliata2000@gmail.com>
2023-05-19 18:53:24 +0000

    19 May 2023 21:53

--- twitter_clone/lib/features/tweet/controller/tweet_controller.dart
+++ twitter_clone/lib/features/tweet/controller/tweet_controller.dart
@@ -24,6 +24,7 @@ class TweetController extends StateNotifier<bool> {
   void _shareImageTweet({
     required List<File> images,
     required String text,
+    required BuildContext context,
   }) {
     
   }

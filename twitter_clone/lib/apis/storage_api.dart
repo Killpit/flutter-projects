7087f8b342094eb9b1ded82434c8b876a269e50b
Ata Tekeli <tekeliata2000@gmail.com>
2023-05-29 12:20:06 +0000

    29 May 2023 15:20

--- twitter_clone/lib/apis/storage_api.dart
+++ twitter_clone/lib/apis/storage_api.dart
@@ -1,6 +1,13 @@
 import 'dart:io';
 import 'package:appwrite/appwrite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/constants/appwrite_constants.dart';
+import 'package:twitter_clone/core/providers.dart';
+
+final storageAPIProvider = Provider((ref) {
+  return StorageAPI(storage: ref.watch(appwriteStorageProvider));
+});
+
 class StorageAPI {
   final Storage _storage;
   StorageAPI({required Storage storage}) : _storage = storage;

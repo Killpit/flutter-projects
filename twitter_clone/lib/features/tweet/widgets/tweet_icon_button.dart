2677a655d3003e44d5ee40f248a03cfcaa0a3df1
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-01 10:26:14 +0000

    1 Haz 2023 13:26

---
+++ twitter_clone/lib/features/tweet/widgets/tweet_icon_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TweetIconButton extends StatelessWidget {
+  final String pathName;
+  final String text;
+  final VoidCallback onTap;
+
+  const TweetIconButton({
+    Key? key,
+    required this.pathName,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

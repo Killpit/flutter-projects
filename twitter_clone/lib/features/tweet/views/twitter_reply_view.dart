9e3706a4d59e5cc7c88d4304c6183d245e08f05c
Ata Tekeli <tekeliata2000@gmail.com>
2023-06-02 11:23:28 +0000

    2 Haz 2023 14:23

---
+++ twitter_clone/lib/features/tweet/views/twitter_reply_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TwitterReplyView extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
